7acb41d6222ffbc0e3740a7006f63091d2f3515a
David-Cornu <97546092+David-Cornu@users.noreply.github.com>
2023-05-05 17:02:36 -0600

    code along

---
+++ quotebook_codealong/Program.cs
@@ -0,0 +1,10 @@
+﻿Board _board = new Board();
+
+_board.AddQuote(
+    new Quote(
+        "Paul",
+        "God hath not given us the spirit of fear; but of power, and of love, and of a sound mind.",
+        new Source("2 Timothy 1:7")
+    ) 
+);
+
